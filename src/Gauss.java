76f39014331e0c7a57db6109273b1790879306c7
Denis Belobrotski <belobrotskiy@gmail.com>
2016-11-18 01:12:14 +0300

    first commit

---
+++ src/Gauss.java
@@ -0,0 +1,179 @@
+class Gauss {
+    private double[][] extendedMtr;
+    private int size = 5;
+    private int[] varNum = {0, 1, 2, 3, 4};
+    private double[] roots = {0, 0, 0, 0, 0};
+    private double[] leadingElements = {0, 0, 0, 0, 0};
+    private double determinant = 0;
+    private double[] residualVector = {0, 0, 0, 0, 0};
+
+    public Gauss(double[][] mtrA, double[] freeMembers) {
+        extendedMtr = new double[5][6];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                extendedMtr[i][j] = mtrA[i][j];
+            }
+            extendedMtr[i][5] = freeMembers[i];
+        }
+    }
+
+    public void printMatrix() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size + 1; j++) {
+                System.out.print(extendedMtr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void printVarNum() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(varNum[i] + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public void printRoots() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(roots[i] + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public void printLeadingElements() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(leadingElements[i] + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public void printDeterminant() {
+        System.out.println(determinant + "\n");
+    }
+
+    public void printResidualVector() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(residualVector[i] + " ");
+        }
+        System.out.println("\n");
+    }
+
+    private int findMaxInString(int strIndex) {
+        int maxIndex = 0;
+        for (int i = 0; i < size; i++) {
+            if (Math.abs(extendedMtr[strIndex][maxIndex]) < Math.abs(extendedMtr[strIndex][i])) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    private void changeColumns(int k, int q) {
+        double temp;
+        int tmp;
+        for (int i = 0; i < size; i++) {
+            temp = extendedMtr[i][k];
+            extendedMtr[i][k] = extendedMtr[i][q];
+            extendedMtr[i][q] = temp;
+            tmp = varNum[k];
+            varNum[k] = varNum[q];
+            varNum[q] = tmp;
+        }
+    }
+
+    public void matrixReduction() {
+        int k;
+        int tmp;
+        for (int i = 0; i < size; i++) {
+            k = findMaxInString(i);
+            if (k != i) {
+                changeColumns(k, i);
+            }
+        }
+    }
+
+    private double[] subtractArrays(double[] a, double[] b) {
+        for (int i = 0; i < size + 1; i++) {
+            a[i] -= b[i];
+        }
+        return a;
+    }
+
+    private double[] divideArray(double[] a, double divider) {
+        for (int i = 0; i < size + 1; i++) {
+            a[i] /= divider;
+        }
+        return a;
+    }
+
+    private double[] multiplyArray(double[] a, double multiplier) {
+        for (int i = 0; i < size + 1; i++) {
+            a[i] *= multiplier;
+        }
+        return a;
+    }
+
+    private double[] copyString(double[] a, double[] b) {
+        for (int i = 0; i < size + 1; i++) {
+            a[i] = b[i];
+        }
+        return a;
+    }
+
+    public void convertToTriangle() {
+        double[] temp = new double[size + 1];
+        for (int i = 0; i < size; i++) {
+            leadingElements[i] = extendedMtr[i][i];
+            extendedMtr[i] = divideArray(extendedMtr[i], extendedMtr[i][i]);
+            for (int j = i + 1; j < size; j++) {
+                temp = copyString(temp, extendedMtr[i]);
+                temp = multiplyArray(temp, extendedMtr[j][i]);
+                extendedMtr[j] = subtractArrays(extendedMtr[j], temp);
+            }
+        }
+    }
+
+    public void calcRoots() {
+        for (int i = 0; i < size; i++) {
+            roots[varNum[size - 1 - i]] = extendedMtr[size - 1 - i][size];
+            for (int j = 0; j < i; j++) {
+                roots[varNum[size - 1 - i]] -= roots[varNum[size - 1 - j]] * extendedMtr[size - 1 - i][size - 1 - j];
+            }
+        }
+    }
+
+    public void calcDeterminant() {
+        double det = 1;
+        for (int i = 0; i < size; i++) {
+            det *= leadingElements[i];
+        }
+        determinant = det;
+    }
+
+    public void calcResidualVector() {
+        double[][] mtrA = new double[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                mtrA[i][j] = extendedMtr[i][j];
+            }
+        }
+        double[][] mtrX = new double[size][1];
+        for (int i = 0; i < size; i++) {
+            mtrX[i][0] = roots[i];
+        }
+        double[][] mtrB = new double[size][1];
+        for (int i = 0; i < size; i++) {
+            mtrB[i][0] = extendedMtr[i][5];
+        }
+        double[][] res = Main.mulMatrix(mtrA, size, size, mtrX, 1);
+        res = Main.subMatrix(res, mtrB, size, 1);
+        for (int i = 0; i < size; i++) {
+            residualVector[i] = res[i][0];
+        }
+    }
+
+    public double[] getRoots() {
+        return roots;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,119 @@
+public class Main {
+
+    private static double[][] mtrA = {{0.6137, -0.0808, 0.0162, 0.0323, 0.1131},
+            {0.0840, 0.9609, 0, -0.0646, 0.0646}, {0.0485, 0, 0.7720, -0.2261, 0.1292},
+            {-0.0969, 0.2035, 0, 0.7591, -0.0323}, {0.4038, 0, 0.1454, 0.0162, 0.9044}};
+    private static double[] freeMembers = {-3.4561, 2.9603, 2.8036, -2.0058, 2.3256};
+    private static int size = 5;
+
+    public static void main(String[] args) {
+        gaussMethod();
+        printInverseMatrix();
+
+    }
+
+    private static void gaussMethod() {
+        Gauss gauss = new Gauss(mtrA, freeMembers);
+        System.out.println("Original matrix:\n");
+        gauss.printMatrix();
+        System.out.println("Variable numbers before reduction:\n");
+        gauss.printVarNum();
+        gauss.matrixReduction();
+        System.out.println("Matrix after reduction:\n");
+        gauss.printMatrix();
+        System.out.println("Variable numbers after reduction:\n");
+        gauss.printVarNum();
+        gauss.convertToTriangle();
+        System.out.println("Triangle matrix:\n");
+        gauss.printMatrix();
+        gauss.calcRoots();
+        System.out.println("Roots:\n");
+        gauss.printRoots();
+        System.out.println("Leading elements:\n");
+        gauss.printLeadingElements();
+        gauss.calcDeterminant();
+        System.out.println("Determinant:\n");
+        gauss.printDeterminant();
+        gauss.calcResidualVector();
+        System.out.println("Residual vector:\n");
+        gauss.printResidualVector();
+    }
+
+    private static void printInverseMatrix() {
+        double[][] inverseMatrix = getInverseMatrix();
+        System.out.println("Inverse matrix (A^(-1)):\n");
+        printMatrix(inverseMatrix, size, size);
+        double[][] mulMtr = mulMatrix(mtrA, size, size, inverseMatrix, size);
+        System.out.println("A * A^(-1) = \n");
+        printMatrix(mulMtr, size, size);
+    }
+
+    private static double[][] getInverseMatrix() {
+        double[][] inverseMatrix = new double[size][size];
+        double[][] identityMatrix = new double[size][size];
+        for (int i = 0; i < size; i++) {
+            identityMatrix[i][i] = 1;
+        }
+        for (int i = 0; i < size; i++) {
+            Gauss gauss = new Gauss(mtrA, identityMatrix[i]);
+            gauss.matrixReduction();
+            gauss.convertToTriangle();
+            gauss.calcRoots();
+            double[] tmpRoots = gauss.getRoots();
+            for (int j = 0; j < size; j++) {
+                inverseMatrix[i][j] = tmpRoots[j];
+            }
+        }
+        inverseMatrix = transposeMatrix(inverseMatrix);
+        return inverseMatrix;
+    }
+
+    public static double[][] mulMatrix(double[][] a, int n1, int m1, double[][] b, int m2) {
+        double[][] c = new double[n1][m2];
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < m2; j++) {
+                for (int k = 0; k < m1; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return c;
+    }
+
+    public static double[][] subMatrix(double[][] a, double[][] b, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] -= b[i][j];
+            }
+        }
+        return a;
+    }
+
+    private static void printArray(double[] a, int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    private static void printMatrix(double[][] mtr, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(mtr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private static double[][] transposeMatrix(double[][] mtr) {
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                double tmp = mtr[i][j];
+                mtr[i][j] = mtr[j][i];
+                mtr[j][i] = tmp;
+            }
+        }
+        return mtr;
+    }
+
+}
